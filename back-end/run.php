b363d3efb3d0882b21a5cf0a1820543e3d71e9bf
Mohammed Alosaimi <mohammedalosaimi@gmail.com>
2021-06-22 22:39:23 +0300

    used to inset into the database

---
+++ back-end/run.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once "config.php";
+
+$run = $_POST["run"];
+
+//check if the database have data befor 
+$sql = "SELECT * FROM moves WHERE id = 1";
+//performe the query
+$result = mysqli_query($conn,$sql);
+//check the number of rows in the table, if zero, this this the first data insert
+$count = mysqli_num_rows($result);
+
+
+//if database have data before then update. otherwise insert     
+if($count == 1) {
+    $sql = "UPDATE moves SET run='$run' WHERE id=1";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully (run = " . $run .")";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn) . " ";
+    }
+} else{
+    $stop = 'stop';
+    $sql = "INSERT INTO moves(run) VALUES ('$run')";
+
+    if(mysqli_query($conn, $sql)){
+        echo "Data has been successfully insert into the record (run = " . $run .")";
+    } else {
+        echo "Error inserting record: ". mysqli_error($conn) . ")";
+    }
+}
+?>
