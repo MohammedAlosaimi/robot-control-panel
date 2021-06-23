00c70abf30b923e90649ffa913ac6b11b329b376
Mohammed Alosaimi <mohammedalosaimi@gmail.com>
2021-06-23 09:07:21 +0300

    Check if the robot is already turned on or not

--- front-end/index.js
+++ front-end/index.js
@@ -5,20 +5,23 @@ const right = document.getElementById("right");
 const backward = document.getElementById("backward");
 const arabic = document.getElementById("arabic");
 const english = document.getElementById("english");
-console.log(run.title);
 
+// when submit move
 function submitMove( move ){
     $.post("/phpinvisualStudioCode/robot-control-panel/back-end/moves.php", {moves: move}, function(data){
         return confirm( data);
     });
 }
 
+// when submit the start/stop
 function submitRun(){
     var action = run.title;
     if(action == "start"){
         run.title = "stop";
+        run.style.border = "3px solid red";
     } else{
         run.title = "start";
+        run.style.border = "3px solid green";
     }
 
     $.post("/phpinvisualStudioCode/robot-control-panel/back-end/run.php", {run: action}, function(data){
@@ -42,11 +45,10 @@ function arLanguage(){
   
     if(run.title == "start"){
         run.innerHTML = "تشغيل";
-        run.title = "start";
-        run.style.border = "3px solid green;"
+        run.style.border = "3px solid green";
     } else{
-        run.innerHTML = "stop";
-        run.style.border = "3px solid red;"
+        run.innerHTML = "ايقاف";
+        run.style.border = "3px solid red";
     }
 
     //hide the (Engilsh) on the top of the page
@@ -65,13 +67,35 @@ function enLanguage(){
 
     if(run.title == "start"){
         run.innerHTML = "start";
-        run.style.border = "3px solid green;";
+        run.style.border = "3px solid green";
     } else {
         run.innerHTML = "stop";
         run.style.border = "3px solid red";
     }
 
-    //hide the() on the top of the page
+    //hide the(arabic word) on the top of the page
     arabic.classList.remove("hidden");
     english.classList.add("hidden")
 }
+
+//Check if the robot is already turned on or not
+fetch("/PHPinVisualStudioCode/robot-control-panel/back-end/runInfo.php").then(
+    function(response){
+        return response.json();
+    }
+).then(function (response){
+    var runInfo = response;
+    console.log(runInfo);
+    if(runInfo == "start"){
+        run.title = "stop";
+        run.innerHTML = "stop";
+        run.style.border = "3px solid red";
+    } else{
+        run.title = "start";
+        run.innerHTML = "start";
+        run.style.border = "3px solid green";
+    }
+})
+.catch(err => {
+    console.error(err);
+});
